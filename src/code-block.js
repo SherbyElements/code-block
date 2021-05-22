bbc985cb7e0b446fb5e79bd4b903943e04a09b82
Benjamin Rancourt <benjamin.rancourt@usherbrooke.ca>
2021-05-22 12:57:29 -0400

    Move the sources to src folder

--- src/code-block.js
+++ src/code-block.js
@@ -1,4 +1,4 @@
-import {LitElement, html, css} from 'lit';
+import {css, html, LitElement} from 'lit';
 import 'prismjs/prism.js';
 import {getLanguageOfAlias} from "./get-language-of-alias";
 import {getLanguageName} from "./get-language-name";
@@ -6,18 +6,10 @@ import {getLanguageName} from "./get-language-name";
 class CodeBlock extends LitElement {
   static get properties() {
     return {
-      language: {
-        type: String,
-      },
-      languageFileTemplate: {
-        type: String,
-      },
-      theme: {
-        type: String,
-      },
-      themeFileTemplate: {
-        type: String,
-      },
+      language: {type: String,},
+      languageFileTemplate: {type: String,},
+      theme: {type: String,},
+      themeFileTemplate: {type: String,},
     };
   }
 
@@ -30,25 +22,27 @@ class CodeBlock extends LitElement {
   }
 
   async __loadLanguage() {
-    const languageFile = this.languageFileTemplate.replace("{LANGUAGE}", getLanguageOfAlias(this.language))
+    const languageFile = this.languageFileTemplate.replace("{LANGUAGE}",
+        getLanguageOfAlias(this.language))
     await import(languageFile);
   }
 
   async firstUpdated() {
     await this.__loadLanguage();
+
     const nodes = this.shadowRoot.querySelector('#code').assignedNodes();
     let codeCombined = '';
     for (let index = 0, len = nodes.length; index < len; ++index) {
       codeCombined += nodes[index].nodeValue;
     }
 
-    // strip the lead/end newlines so we don't look horrible
+    // Strip the lead/end newlines so they don't look horrible
+    const grammar = Prism.languages[this.language];
     const codeClean = codeCombined.replace(/^\s+|\s+$/g, '');
-    const highlight = Prism.highlight(codeClean, Prism.languages[this.language],
-        this.language);
 
     // Set to our styled block
-    this.shadowRoot.querySelector('#output').innerHTML = highlight;
+    this.shadowRoot.querySelector('#output').innerHTML = Prism.highlight(
+        codeClean, grammar, this.language);
   }
 
   static get styles() {
@@ -68,7 +62,7 @@ class CodeBlock extends LitElement {
       pre:before {
         content: attr(data-language-name);
         color: var(--code-block-language-color);
-        font-family: -apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+        font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Helvetica, Arial, sans-serif, Apple Color Emoji, Segoe UI Emoji, Segoe UI Symbol;
         font-weight: 700;
         letter-spacing: 1px;
         position: absolute;
@@ -77,22 +71,22 @@ class CodeBlock extends LitElement {
         z-index: 1;
       }
     `;
+  }
+
+  render() {
+    const themeFile = this.themeFileTemplate.replace("{THEME}", this.theme)
+    return html`
+      <link rel="stylesheet" href="${themeFile}">
+      <pre
+          class="language-${this.language} line-numbers"
+          data-language-name="${getLanguageName(this.language)}"
+      ><code id="output"></code></pre>
+
+      <div id="hide">
+        <slot id="code"></slot>
+      </div>
+    `;
+  }
 }
 
-render()
-{
-  const themeFile = this.themeFileTemplate.replace("{THEME}", this.theme)
-  return html`
-    <link rel="stylesheet" href="${themeFile}">
-    <pre 
-        class="language-${this.language}"
-        data-language-name="${getLanguageName(this.language)}"
-    ><code id="output"></code></pre>
-
-    <div id="hide">
-      <slot id="code"></slot>
-    </div>
-  `;
-}
-}
 customElements.define('code-block', CodeBlock);
